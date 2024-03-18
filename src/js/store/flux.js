4dd171bc03b1e49cbc242732b2334288c25a22b7
QZE7 <68819710+QZE7@users.noreply.github.com>
2024-03-18 20:28:20 +0000

    Inicio

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,43 +1,23 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
+			planet: [],
+			character: []
 		},
 		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
+			getPlanets: async () => {
+				fetch('https://www.swapi.tech/api/planets/')
+					.then(res => res.json())
+					.then(data => setStore({ planet: data.results }));
 			},
-			loadSomeData: () => {
-				/**
-					fetch().then().then(data => setStore({ "foo": data.bar }))
-				*/
-			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
+			getCharacters: async () => {
+				fetch('https://www.swapi.tech/api/people')
+					.then(res => res.json())
+					.then(data => setStore({ character: data.results }));
+					
 
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
-
-				//reset the global store
-				setStore({ demo: demo });
 			}
+			
 		}
 	};
 };
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,69 @@
-import React from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useContext, useEffect } from "react";
+//import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
+import { Context } from "../store/appContext";
+import { Link } from "react-router-dom";
+import { MdOutlineFavoriteBorder } from "react-icons/md";
 
-export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
-	</div>
-);
+
+export const Home = () => {
+
+	useEffect(() => {
+		actions.getPlanets();
+	}, []);
+
+	useEffect(() => {
+		actions.getCharacters();
+	}, []);
+
+	const { store, actions } = useContext(Context);
+
+	return (
+		<div className="w-100%">
+			<h3 className="ps-5">Planets</h3>
+			<div className="caja-horizontal">
+				{store.planet.map((planet, index) => (
+					<div className="card width-p col-auto" key={index}>
+						<img src="..." className="card-img-top" alt="..." />
+						<div className="card-body">
+							<h5 className="card-title">{planet.name}</h5>
+							<p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+							<div className="d-flex justify-content-between">
+								<div>
+									<a href="#" className="btn btn-primary ">More infomation</a>
+								</div>
+								<div>
+									<button href="#"><MdOutlineFavoriteBorder /></button>
+								</div>
+							</div>
+						</div>
+					</div>
+				))}
+			</div>
+			<br></br>
+			<h3 className="ps-5">Character</h3>
+			<div className="caja-horizontal">
+				{store.character.map((character, index) => (
+					<div className="card  width-p col-auto" key={index}>
+						<img src="..." className="card-img-top" alt="..." />
+						<div className="card-body">
+							<h5 className="card-title">{character.name}</h5>
+							<p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+							<div className="d-flex justify-content-between">
+								<div>
+									<a href="#" className="btn btn-primary ">More infomation</a>
+								</div>
+								<div>
+
+									<button href="#"><MdOutlineFavoriteBorder /></button>
+								</div>
+							</div>
+						</div>
+					</div>
+				))}
+			</div>
+		</div>
+
+	);
+
+};
